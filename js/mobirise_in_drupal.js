44da00be66f9fa9d32fbe2ceb43659bfc1d500a0
dataweb <bert@data-web.be>
2022-11-09 12:57:07 +0100

    Refactor the JS code a bit to make it more robust.

--- js/mobirise_in_drupal.js
+++ js/mobirise_in_drupal.js
@@ -7,25 +7,53 @@
     }
   };
 
+  const myTimeout = setTimeout(positionNavigation, 100);
+
+  $(window).resize(function() {
+    positionNavigation();
+  });
+
   $(window).scroll(function() {
+    positionNavigation();
+  });
+
+
+  function positionNavigation() {
     // If drupal admin toolbar is in place.
     if ($('.toolbar-bar').length > 0) {
-      // And mobirise header is in place too.
-      if ($('header .top-header').length > 0) {
-        // Stick to the bottom of the admin toolbar.
-        let headerTop = $('header .top-header').offset().top;
-        let adminToolbarHeight = $('.toolbar-bar').height() + $('.toolbar-tray-horizontal.is-active').height();
+      // Determine the admin toolbar height.
+      let toolbarTrayHeight = 0;
+      if ($('.toolbar-tray-horizontal').hasClass('is-active')) {
+        toolbarTrayHeight = $('.toolbar-tray-horizontal').height();
+      }
+      let adminToolbarHeight = $('.toolbar-bar').height() + toolbarTrayHeight;
+
+      // Correct the body top padding.
+      $('body').css('padding-top', adminToolbarHeight);
+
+      // Position the Mobirise to there is a navbar in place.
+      if ($('nav.navbar').length > 0) {
+        let headerTop;
+        // Position the Mobirise navbar to the header top position if a header is in place.
+        if ($('header .top-header').length > 0) {
+          headerTop = $('header .top-header').offset().top;
+        }
+        else {
+          // Position the Mobirise navbar at under the admin toolbar.
+          headerTop = adminToolbarHeight;
+
+          // Correct the body top padding.
+          $('body').css('padding-top', adminToolbarHeight + $('nav.navbar').height());
+        }
+
         let navTop = headerTop - $(window).scrollTop();
-        if (headerTop > 0) {
-          if (navTop < adminToolbarHeight) {
-            navTop = adminToolbarHeight + 1;
-          }
+        if ($(window).scrollTop() > (adminToolbarHeight)) {
+          navTop = adminToolbarHeight;
         }
         $('nav.navbar').css('top', navTop);
-      } else {
-        $('nav.navbar').css('position', 'relative');
       }
     }
-  });
+  }
+
 
 })(jQuery);
